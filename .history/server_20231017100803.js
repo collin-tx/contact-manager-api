df19433e392c61268a0f8d5beb428f25aa82183f
Collin <collin.smith@7summitsinc.com>
2023-10-18 10:36:37 -0600

    setup

---
+++ .history/server_20231017100803.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/api/contacts', (req, res) => {
+    res.send("Get All Contacts");
+});
+
+app.listen(port, () => {
+    console.log('server running on port ' + port);
+});
---
+++ .history/server_20231017101616.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+// app.get('/api/contacts', (req, res) => {
+//     res.status(200).json({message: "Get All Contacts"});
+// });
+
+app.use("/api/contacts", require("./routes/contactRoutes"));
+
+app.listen(port, () => {
+    console.log('server running on port ' + port);
+});
